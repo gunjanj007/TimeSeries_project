37c20b10191649b16ba6c8e4b95aa7229dc46c92
kushagragpt99 <glaedur@inheritance.localdomain>
2020-11-29 02:57:52 +0530

    adding all R plots

--- bitcoin.R
+++ bitcoin.R
@@ -151,34 +151,46 @@ train_ethereum <- ethereum_data[row.num,]
 test_ethereum <- ethereum_data[20:1,]
 
 #plot of train dataset
+pdf('trace_train_Bitcoin.pdf')
 ggplot(train_bitcoin, aes(train_bitcoin$Date, train_bitcoin$`Close`)) +
   geom_line(color = 'blue') + scale_x_date("year")+ ylim(0,20000) + ylab("Closing Price") + 
-  ggtitle('train_dataset')
+  ggtitle('train_dataset Bitcoin')
+dev.off()
 
 #plot of test dataset
+pdf('trace_test_Bitcoin.pdf')
 ggplot(test_bitcoin, aes(test_bitcoin$Date, test_bitcoin$`Close`)) +
   geom_line(color = 'blue') + scale_x_date("year")+ ylim(3000,4000) + ylab("Closing Price") + 
-  ggtitle('test_dataset')
+  ggtitle('test_dataset Bitcoin')
+dev.off()
 
 #plot of train dataset
+pdf('trace_train_Ethereum.pdf')
 ggplot(train_ethereum, aes(train_ethereum$Date, train_ethereum$`Close`)) +
   geom_line(color = 'blue') + scale_x_date("year") + ylab("Closing Price") + 
-  ggtitle('train_dataset')
+  ggtitle('train_dataset Ethereum')
+dev.off()
 
 #plot of test dataset
+pdf('trace_test_Ethereum.pdf')
 ggplot(test_ethereum, aes(test_ethereum$Date, test_ethereum$`Close`)) +
   geom_line(color = 'blue') + scale_x_date("year") + ylab("Closing Price") + 
   ggtitle('test_dataset')
+dev.off()
 
 
 #forecasting model with closing prices of bitcoin
 bitcoin_time_series  <- ts(train_bitcoin$`Close`,frequency = 365.25,start = c(2013,4,27))
 bitcoin_time_series_decompose <- decompose(bitcoin_time_series)
+pdf('decomp_Bitcoin.pdf')
 plot(bitcoin_time_series_decompose)
+dev.off()
 
 ethereum_time_series  <- ts(train_ethereum$`Close`,frequency = 365.25,start = c(2013,4,27))
 ethereum_time_series_decompose <- decompose(ethereum_time_series)
+pdf('decomp_Ethereum.pdf')
 plot(ethereum_time_series_decompose)
+dev.off()
 
 #It can be seen there is a uniform seasonal variation in the closing price of bitcoins over years and
 #and trend has been almost constant till the end of 2018.
@@ -242,9 +254,11 @@ testdata <- test_bitcoin[1:10,5]
 
 accuracy(holt_df[,1], testdata) #accuracy
 
+pdf('Holt_bitcoin.pdf')
 ggplot() + geom_line(data = holtfdf, aes(Date, holtfdf[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = holtfdf, aes(Date, holtfdf[,8]), color = "Dark Red") + 
   ggtitle('Predicted vs Actual for Holts Method') 
+dev.off()
 
 dim(train_ethereum)
 
@@ -260,9 +274,11 @@ testdata <- test_ethereum[1:10,5]
 
 accuracy(holt_df[,1], testdata) #accuracy
 
+pdf('Holt_ethereum.pdf')
 ggplot() + geom_line(data = holtfdf, aes(Date, holtfdf[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = holtfdf, aes(Date, holtfdf[,8]), color = "Dark Red") + 
   ggtitle('Predicted vs Actual for Holts Method') 
+dev.off()
 
 #The mean accurate prediction error comes out to be 1.47% which is very good as it is under 10%. 
 #Here we can see a major problem that model cannot predict the most of dips in prices of bitcoins.
@@ -294,10 +310,11 @@ accuracy(ETS_df[,1], testdata)
 
 etsfdf <- cbind(test_bitcoin[1:10,], ETS_df[,1])
 
+pdf('ETS_bitcoin.pdf')
 ggplot() + geom_line(data = etsfdf, aes(Date, etsfdf[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = etsfdf, aes(Date, etsfdf[,8]), color = "Dark Red") + 
   ggtitle('Predicted vs Actual for ETS method') 
-
+dev.off()
 
 ETS <- ets((train_ethereum[,'Close']),allow.multiplicative.trend=TRUE)
 ETS_forecast <- forecast(ETS, h = 10)
@@ -314,10 +331,11 @@ accuracy(ETS_df[,1], testdata)
 
 etsfdf <- cbind(test_ethereum[1:10,], ETS_df[,1])
 
+pdf('ETS_ethereum.pdf')
 ggplot() + geom_line(data = etsfdf, aes(Date, etsfdf[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = etsfdf, aes(Date, etsfdf[,8]), color = "Dark Red") + 
   ggtitle('Predicted vs Actual for ETS method') 
-
+dev.off()
 #The mean accurate prediction error comes out to be 0.82% which has improved in accuracy, error rate is 
 #decreased by 0.7
 #But still we can observe that our predicted value is not curving with the data, it still posses linear trend.
@@ -351,9 +369,13 @@ plot(tsdf,type = "l", xlab = 'Closing Price',ylab='Number_of_Days',ylim = c(-500
 
 #Auto- and Cross- Covariance and -Correlation Function Estimation
 #plots the estimates 
+pdf('ACF_bitcoin.pdf')
 acf(tsdf)
+dev.off()
 #pacf is used for partial auto correlation function
-pacf(tsdf)  
+pdf('PACF_bitcoin.pdf')
+pacf(tsdf)
+dev.off()
 
 
 tsdf <- diff(train_ethereum[,5], lag = 2)
@@ -367,9 +389,13 @@ plot(tsdf,type = "l", xlab = 'Closing Price',ylab='Number_of_Days',ylim = c(-500
 
 #Auto- and Cross- Covariance and -Correlation Function Estimation
 #plots the estimates 
+pdf('ACF_ethereum.pdf')
 acf(tsdf)
+dev.off()
 #pacf is used for partial auto correlation function
+pdf('PACF_ethereum.pdf')
 pacf(tsdf)  
+dev.off()
 
 
 #It divides the time series data into models based on time lags in an autoregressive model, the 
@@ -382,6 +408,24 @@ pacf(tsdf)
 
 #The mean accurate prediction error comes out to be 1.50% for Auto ARIMA 
 
+
+auto.arima(train_bitcoin[,5])
+
+bitcoin_time_series_forecast_auto <- forecast(auto.arima(train_bitcoin[,5]), h=10)
+plot(bitcoin_time_series_forecast_auto, ylab = 'Closing Price', xlab = 'Number_of_Days', main='Forecast AUTO-ARIMA (1,1,1)')
+
+bts_f_df_auto <- as.data.frame(bitcoin_time_series_forecast_auto)
+
+testdata <- test_bitcoin[1:10,5]
+accuracy(bts_f_df_auto[,1],testdata)
+
+pdf('auto_arima_bitcoin.pdf')
+gegefct_auto <- cbind(test_bitcoin[1:10,], bts_f_df_auto[,1])
+ggplot() + geom_line(data = gegefct_auto, aes(Date, gegefct_auto[,5]), color = "blue") + ylab('Closing Price') +
+  geom_line(data = gegefct_auto, aes(Date, gegefct_auto[,8]), color = "Dark Red") +  
+  ggtitle('Predicted vs Actual for Auto-ARIMA')
+dev.off()
+
 auto.arima(train_ethereum[,5])
 
 ethereum_time_series_forecast_auto <- forecast(auto.arima(train_ethereum[,5]), h=10)
@@ -392,10 +436,12 @@ bts_f_df_auto <- as.data.frame(ethereum_time_series_forecast_auto)
 testdata <- test_ethereum[1:10,5]
 accuracy(bts_f_df_auto[,1],testdata)
 
+pdf('auto_arima_ethereum.pdf')
 gegefct_auto <- cbind(test_ethereum[1:10,], bts_f_df_auto[,1])
 ggplot() + geom_line(data = gegefct_auto, aes(Date, gegefct_auto[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = gegefct_auto, aes(Date, gegefct_auto[,8]), color = "Dark Red") +  
   ggtitle('Predicted vs Actual for Auto-ARIMA')
+dev.off()
 #The mean accurate prediction error comes out to be 1.50% for Auto ARIMA 
 
 
@@ -411,11 +457,12 @@ bts_f_df <- as.data.frame(bitcoin_time_series_forecast)
 testdata <- test_bitcoin[1:10,5]
 accuracy(bts_f_df[,1],testdata)
 
+pdf('arima_bitcoin.pdf')
 gegefct <- cbind(test_bitcoin[1:10,], bts_f_df[,1])
 plot(bitcoin_time_series_forecast,ylab = 'Closing Price', xlab = 'Number_of_Days', main='Forecast ARIMA (1,1,8)')
 ggplot() + geom_line(data = gegefct, aes(Date, gegefct[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = gegefct, aes(Date, gegefct[,8]), color = "Dark Red") +  ggtitle('Predicted vs Actual for ARIMA')
-
+dev.off()
 
 ethereum_time_series_forecast <- forecast(arima(train_ethereum[,5], order = c(5,1,8)), h=10)
 arima(train_ethereum[,5], order = c(5,1,8))
@@ -428,11 +475,12 @@ bts_f_df <- as.data.frame(ethereum_time_series_forecast)
 testdata <- test_ethereum[1:10,5]
 accuracy(bts_f_df[,1],testdata)
 
+pdf('arima_ethereum.pdf')
 gegefct <- cbind(test_ethereum[1:10,], bts_f_df[,1])
 plot(ethereum_time_series_forecast,ylab = 'Closing Price', xlab = 'Number_of_Days', main='Forecast ARIMA (1,1,8)')
 ggplot() + geom_line(data = gegefct, aes(Date, gegefct[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = gegefct, aes(Date, gegefct[,8]), color = "Dark Red") +  ggtitle('Predicted vs Actual for ARIMA')
-
+dev.off()
 
 #The mean accurate prediction error comes out to be 0.68% for ARIMA in which we set parameters
 #by observing ACF and PACF graph. This can be treated as overfitted as well. 
@@ -440,26 +488,31 @@ ggplot() + geom_line(data = gegefct, aes(Date, gegefct[,5]), color = "blue") + y
 #-----------------------------------------------------------------------------------------------------
 #ALL models line plot
 
-all <- cbind(test_bitcoin[1:10,],bts_f_df[,1],ETS_df[,1],holt_df[,1], bts_f_df_auto[,1])
+all <- cbind(test_bitcoin[1:10,],bts_f_df[,1],ETS_df[,1],holt_df[,1])
 head(all)
 
+pdf('all_bitcoin.pdf')
 ggplot() + geom_line(data = all, aes(Date, all[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = all, aes(Date, all[,8]), color = "green") +  
   geom_line(data = all, aes(Date, all[,9]), color = "red") +  
   geom_line(data = all, aes(Date, all[,10]), color = "orange") +  
-  geom_line(data = all, aes(Date, all[,11]), color = "dark green") +  
   ggtitle('Predicted vs Actual for all methods')
+dev.off()
 
-all <- cbind(test_ethereum[1:10,],bts_f_df[,1],ETS_df[,1],holt_df[,1], bts_f_df_auto[,1])
+all <- cbind(test_ethereum[1:10,],bts_f_df[,1],ETS_df[,1],holt_df[,1])#, bts_f_df_auto[,1])
 head(all)
+colors <- c("Actual" = "blue", "ARIMA" = "green", "ETS" = "red", "Holt's" = "orange")
 
+pdf('all_ethereum.pdf')
 ggplot() + geom_line(data = all, aes(Date, all[,5]), color = "blue") + ylab('Closing Price') +
   geom_line(data = all, aes(Date, all[,8]), color = "green") +  
   geom_line(data = all, aes(Date, all[,9]), color = "red") +  
   geom_line(data = all, aes(Date, all[,10]), color = "orange") +  
-  geom_line(data = all, aes(Date, all[,11]), color = "dark green") +  
-  ggtitle('Predicted vs Actual for all methods')
-
+  #geom_line(data = all, aes(Date, all[,11]), color = "dark green") +  
+  ggtitle('Predicted vs Actual for all methods') +
+  scale_colour_manual(values=colors) +
+  theme(legend.position="right")
+dev.off()
 
 #------------------------------------------
 
